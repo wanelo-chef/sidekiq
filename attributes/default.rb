23434b52ae8fae6954031b31dd48b2f6d2c50e97
Eric Saxby & Paul Henry <pair+sax+paul@wanelo.com>
2012-12-13 15:25:11 -0800

    sidekiq::monitor recipe works (with RVM)

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,3 +7,4 @@ default["sidekiq"]["pid_dir"] = "/var/run/sidekiq"
 
 default["sidekiq"]["monitor"]["user"] = nil
 default["sidekiq"]["monitor"]["group"] = nil
+default["sidekiq"]["monitor"]["rvm"] = "1.9.3-p194"
--- recipes/monitor.rb
+++ recipes/monitor.rb
@@ -19,16 +19,51 @@
 
 user = node["sidekiq"]["monitor"]["user"]
 application_directory = "/home/#{user}/app/current"
+shared_directory = "/home/#{user}/app/shared"
 rails_env = node["sidekiq"]["rails_env"]
 
-sidekiq_monitor_run_path    = "#{application_directory}/sidekiq_monitor.ru"
+rvm_path = "/home/#{user}/.rvm"
+rvm_version = node["sidekiq"]["monitor"]["rvm"]
+
+sidekiq_monitor_run_path = "#{application_directory}/sidekiq_monitor.ru"
 sidekiq_monitor_config_path = "#{application_directory}/config/unicorn/sidekiq_monitor.rb"
+sidekiq_monitor_log_path = "#{shared_directory}/log/sidekiq_monitor.stderr.log"
+
+run_command = "/opt/local/bin/sidekiq-monitor.sh"
+
+environment_variables = {
+    "rvm_path" => "/home/#{user}/.rvm",
+    "rvm_bin_path" => "/home/#{user}/.rvm/bin",
+    "rvm_prefix" => "/home/#{user}",
+    "TERM" => "xterm",
+    "PATH" => "/opt/local/bin:/opt/local/sbin:/usr/bin:/usr/sbin:/home/#{user}/.rvm/bin",
+    "GEM_HOME" => "/home/#{user}/.rvm/gems/#{rvm_version}"
+}
+
+template run_command do
+  source "sidekiq-monitor.sh.erb"
+  cookbook "sidekiq"
+  mode 0755
+end
 
 smf "sidekiq-monitor" do
-  user node["sidekiq"]["monitor"]["user"]
+  cmd = "#{run_command} -c #{sidekiq_monitor_config_path} -e #{rails_env} -r #{sidekiq_monitor_run_path} -R #{rvm_path}"
+
+  user user
   group node["sidekiq"]["monitor"]["group"]
-  start_command "(BUNDLE_GEMFILE=#{application_directory}/Gemfile bundle exec unicorn -c #{sidekiq_monitor_config_path} -E #{rails_env} -D #{sidekiq_monitor_run_path} 2>&1)"
-  start_timeout 30
+
+  start_command cmd
+  start_timeout 60
+
   stop_timeout 15
   working_directory application_directory
+
+  environment(environment_variables)
+  property_groups(
+      "config" => {
+          "rails_env" => rails_env,
+          "config_file" => sidekiq_monitor_config_path,
+          "log_file" => sidekiq_monitor_log_path
+      }
+  )
 end
